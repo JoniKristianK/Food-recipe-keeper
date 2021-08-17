00eef5705fcae793cd028fc52d15df02a0f135ec
JoniKristianK <joni.karilainen@gmail.com>
2021-08-17 09:37:11 +0300

    Add: custom fonts

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading'; // expo install expo-app-loading
+import { useFonts } from 'expo-font';
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>FOOD RECIPE KEEPER</Text>
-		</View>
-	);
+	let [fontsLoaded] = useFonts({
+		'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+		'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+	});
+
+	if (!fontsLoaded) {
+		return <AppLoading />;
+	} else {
+		return (
+			<View style={styles.container}>
+				<Text>FOOD RECIPE KEEPER</Text>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
