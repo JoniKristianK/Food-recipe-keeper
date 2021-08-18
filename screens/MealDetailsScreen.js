ecff0aa1e99c8f656e6f2c3633f1abfb5fea13ad
JoniKristianK <joni.karilainen@gmail.com>
2021-08-18 09:17:21 +0300

    Add: favorites header button (start)

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import { MEALS } from '../data/dummy-data';
+import HeaderButton from '../components/HeaderButton';
 
 const MealsDetailsScreen = ({ navigation }) => {
 	const mealId = navigation.getParam('mealId');
@@ -24,6 +26,15 @@ MealsDetailsScreen.navigationOptions = (navigationData) => {
 	const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 	return {
 		headerTitle: selectedMeal.title,
+		headerRight: (
+			<HeaderButtons HeaderButtonComponent={HeaderButton}>
+				<Item
+					title='Favorite'
+					iconName={'ios-star'}
+					onPress={() => console.log('buu')}
+				/>
+			</HeaderButtons>
+		),
 	};
 };
 
