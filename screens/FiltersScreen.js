23a60764674cec40cd61fbc3edf55da9493f8d1d
JoniKristianK <joni.karilainen@gmail.com>
2021-08-18 16:58:48 +0300

    Add: FilterScreen navigationOptions

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
-const FiltersScreen = () => {
+import HeaderButton from '../components/HeaderButton';
+
+const FiltersScreen = (props) => {
 	return (
 		<View style={styles.screen}>
-			<Text>FiltersScreen</Text>
+			<Text>The Filters Screen!</Text>
 		</View>
 	);
+};
+
+FiltersScreen.navigationOptions = (navData) => {
+	return {
+		headerTitle: 'Filter Meals',
+		headerLeft: () => (
+			<HeaderButtons HeaderButtonComponent={HeaderButton}>
+				<Item
+					title='Menu'
+					iconName='ios-menu'
+					onPress={() => {
+						navData.navigation.toggleDrawer();
+					}}
+				/>
+			</HeaderButtons>
+		),
+	};
 };
 
 const styles = StyleSheet.create({
