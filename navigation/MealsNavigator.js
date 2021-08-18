a6e804ca10e1050db5439525d5ffc3a22995f9e0
JoniKristianK <joni.karilainen@gmail.com>
2021-08-18 09:53:39 +0300

    Add: bottom navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,10 +1,12 @@
 import { Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createAppContainer } from 'react-navigation';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailsScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
 import Colors from '../constants/Colors';
 
 const MealsNavigator = createStackNavigator(
@@ -31,4 +33,9 @@ const MealsNavigator = createStackNavigator(
 	}
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator({
+	Meals: MealsNavigator,
+	Favorites: FavoritesScreen,
+});
+
+export default createAppContainer(MealsFavTabNavigator);
