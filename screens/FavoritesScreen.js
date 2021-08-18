b0b5bd4628cf3605a3125a0619e518744b9571dd
JoniKristianK <joni.karilainen@gmail.com>
2021-08-18 16:59:50 +0300

    Add: FavoritesScreen navigationOptions

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
+import HeaderButton from '../components/HeaderButton';
 import MealList from '../components/MealList';
 import { MEALS } from '../data/dummy-data';
 
@@ -10,8 +12,21 @@ const FavoritesScreen = ({ navigation }) => {
 	return <MealList listData={favMeals} navigation={navigation} />;
 };
 
-FavoritesScreen.navigationOptions = {
-	headerTitle: 'Your Favorites',
+FavoritesScreen.navigationOptions = (navData) => {
+	return {
+		headerTitle: 'Your Favorites',
+		headerLeft: () => (
+			<HeaderButtons HeaderButtonComponent={HeaderButton}>
+				<Item
+					title='Menu'
+					iconName='ios-menu'
+					onPress={() => {
+						navData.navigation.toggleDrawer();
+					}}
+				/>
+			</HeaderButtons>
+		),
+	};
 };
 
 export default FavoritesScreen;
