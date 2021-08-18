9ffbc0a79acc7856a0b089268b1260183ea8341c
JoniKristianK <joni.karilainen@gmail.com>
2021-08-18 12:59:15 +0300

    Add: MealList component

---
+++ components/MealList.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { View, FlatList, StyleSheet } from 'react-native';
+
+import MealItem from './MealItem';
+
+const MealList = ({ listData, navigation }) => {
+	const renderMealItem = (itemData) => {
+		return (
+			<MealItem
+				title={itemData.item.title}
+				image={itemData.item.imageUrl}
+				duration={itemData.item.duration}
+				complexity={itemData.item.complexity}
+				affordability={itemData.item.affordability}
+				onSelectMeal={() => {
+					navigation.navigate({
+						routeName: 'MealDetail',
+						params: {
+							mealId: itemData.item.id,
+						},
+					});
+				}}
+			/>
+		);
+	};
+	return (
+		<View style={styles.list}>
+			<FlatList
+				data={listData}
+				keyExtractor={(item, index) => item.id}
+				renderItem={renderMealItem}
+				style={{ width: '100%' }}
+			/>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	list: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		padding: 15,
+	},
+});
+
+export default MealList;
