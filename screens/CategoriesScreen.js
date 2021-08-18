390f7fb71e44febf753a2ceb802cf07d2baf6a55
JoniKristianK <joni.karilainen@gmail.com>
2021-08-18 16:57:53 +0300

    Add: Catagories navigationOptions

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -6,7 +6,9 @@ import {
 	StyleSheet,
 	TouchableOpacity,
 } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
+import HeaderButton from '../components/HeaderButton';
 import { CATEGORIES } from '../data/dummy-data';
 import CategoryGridTile from '../components/CategoryGridTile';
 
@@ -38,8 +40,21 @@ const CategoriesScreen = ({ navigation }) => {
 	);
 };
 
-CategoriesScreen.navigationOptions = {
-	headerTitle: 'Meal Categories',
+CategoriesScreen.navigationOptions = (navData) => {
+	return {
+		headerTitle: 'Meal Categories',
+		headerLeft: () => (
+			<HeaderButtons HeaderButtonComponent={HeaderButton}>
+				<Item
+					title='Menu'
+					iconName='ios-menu'
+					onPress={() => {
+						navData.navigation.toggleDrawer();
+					}}
+				/>
+			</HeaderButtons>
+		),
+	};
 };
 
 export default CategoriesScreen;
