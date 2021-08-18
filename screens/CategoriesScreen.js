737d536c7d3d6e8a5bc9aaed41ac7d374fd4df34
JoniKristianK <joni.karilainen@gmail.com>
2021-08-18 17:02:14 +0300

    cleaning

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-	View,
-	Text,
-	FlatList,
-	StyleSheet,
-	TouchableOpacity,
-} from 'react-native';
+import { FlatList } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../components/HeaderButton';
