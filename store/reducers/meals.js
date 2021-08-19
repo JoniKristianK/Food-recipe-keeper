2b793db6d0f744f39fd2cb152f3d629871061049
JoniKristianK <joni.karilainen@gmail.com>
2021-08-19 10:08:18 +0300

    Add: filters reducer

--- store/reducers/meals.js
+++ store/reducers/meals.js
@@ -1,5 +1,5 @@
 import { MEALS } from '../../data/dummy-data';
-import { TOGGLE_FAVORITE } from '../actions/meals';
+import { TOGGLE_FAVORITE, SET_FILTERS } from '../actions/meals';
 
 const initialState = {
 	meals: MEALS,
@@ -30,6 +30,28 @@ const mealsReducer = (state = initialState, action) => {
 					favoriteMeals: state.favoriteMeals.concat(meal),
 				};
 			}
+		// If meal is set to false --> drop it from the array
+		case SET_FILTERS:
+			const appliedFilters = action.filters;
+			const updatedFilteredMeals = state.meals.filter((meal) => {
+				if (appliedFilters.glutenFree && !meal.isGlutenFree) {
+					return false;
+				}
+				if (appliedFilters.lactoseFree && !meal.isLactoseFree) {
+					return false;
+				}
+				if (appliedFilters.vegan && !meal.isVegan) {
+					return false;
+				}
+				if (appliedFilters.vegetarian && !meal.isVegetarian) {
+					return false;
+				}
+				return true;
+			});
+			return {
+				...state,
+				filteredMeals: updatedFilteredMeals,
+			};
 		default:
 			return state;
 	}
